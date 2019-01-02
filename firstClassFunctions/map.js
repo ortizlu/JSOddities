48360ebd527ebf6387f3072ef9451590da7d9195
Luis Ortiz <ortizlu@icloud.com>
2019-01-02 00:06:53 -0500

    added bind to set default params

--- firstClassFunctions/map.js
+++ firstClassFunctions/map.js
@@ -11,3 +11,10 @@ let arr = [1, 2, 3];
 console.log(mapEach(arr, item => item > 2));
 
 //========================================================================
+
+let checkPastLimit = function(limiter, item) {
+  return item > limiter;
+};
+
+//used bind to set a default parameter. In this case, we set the limiter to be 1. So our mapEach function checks whether each number is greater than 1
+console.log(mapEach(arr, checkPastLimit.bind(this, 1)));
