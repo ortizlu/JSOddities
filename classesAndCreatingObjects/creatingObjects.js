a22c704d60a0c0be97909885915ddc3fffc1dd94
Luis Ortiz <ortizlu@icloud.com>
2019-01-02 14:48:58 -0500

    added some more stuff in the creating objects readme

--- classesAndCreatingObjects/creatingObjects.js
+++ classesAndCreatingObjects/creatingObjects.js
@@ -1,10 +1,6 @@
 //creating a class in the past
 function Animal(name) {
   this.name = name;
-
-  this.whoAmI = function() {
-    console.log(`my name is ${this.name}`);
-  };
 }
 
 //"new" creates a new object from that function
@@ -12,7 +8,29 @@ function Animal(name) {
 let dog = new Animal('scruffles');
 
 console.log(dog.name); //scruffles
+
+//we also see that the prototype is now the Animal prototype (object)
+
+console.log(dog.__proto__); // Animal{}
+
+//but where does this prototype come from? Well it comes from the "prototype" property of Animal
+
+console.log(Animal.prototype); //Animal{}
+
+//so is the "prototype" property the actual prototype of the Animal function?
+
+console.log(Animal.__proto__); //[Function]
+
+//No. Every function has a "prototype" property so when the "new" keyword is used, it gives it to that new object
+
+//The "prototype" from the Animal function starts its life as an empty object, but we can attach properties to it
+Animal.prototype.whoAmI = function() {
+  console.log(`my name is ${this.name}`);
+};
+
+//and then we can use it with the dog because of the prototype chain
 console.log(dog.whoAmI()); //my name is scruffles
 
-//we see that the prototype is now the Animal prototype (object)
-console.log(dog.__proto__); // Animal{}
+//why can't we just add it to Animal using
+//this.whoAmI = ... ?
+//we can, but since objects take up memory space, whenever a new Animal is created, a new copy of that whoAmI property will be created. So we will be using a lot of memory for no reason.
