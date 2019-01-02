477b008d42c145156e4866d208c5f2104ff9e83d
Luis Ortiz <ortizlu@icloud.com>
2019-01-02 00:38:11 -0500

    added own filter function

---
+++ firstClassFunctions/filter.js
@@ -0,0 +1,15 @@
+function filterEach(arr, cb) {
+  let returnedArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (cb(arr[i]) === true) {
+      returnedArr.push(arr[i]);
+    }
+  }
+  return returnedArr;
+}
+
+//filter all of the 3's
+let filterMe = [1, 2, 3, 5, 2, 3, 7, 3, 8, 9, 3, 10, 3];
+
+let filtered = filterEach(filterMe, number => number !== 3);
+console.log(filtered);
